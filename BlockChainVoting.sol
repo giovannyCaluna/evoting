a17160e7e3545e64643cfe07d4cc3ce193ce6873
giovannyCaluna <gio-vanny1@hotmail.com>
2024-04-30 00:29:21 +0200

    publishing the results

--- BlockChainVoting.sol
+++ BlockChainVoting.sol
@@ -69,8 +69,9 @@ contract BlockchainVoting {
 
         bool candidateFound = true;
         for (uint256 i = 0; i < candidates.length; i++) {
-            if (candidates[0].candidateAddress == _candidateAddress) {
+            if (candidates[i].candidateAddress == _candidateAddress) {
                 uint256 randomNumber = getRandomNormalized();
+                //if the random number < epsilon we add the vote to the candidate 1
                 if (randomNumber < epsilon) {
                     candidates[0].voteCount++;
                     voters.push(
@@ -83,13 +84,14 @@ contract BlockchainVoting {
                         )
                     );
                 } else {
-                    candidates[1].voteCount++;
+                    // if the random number isnot < epsilon we keep the original vote. 
+                    candidates[i].voteCount++;
                     voters.push(
                         Voter(
                             _Id,
                             _name,
                             _voterAddress,
-                            candidates[1].candidateAddress,
+                            candidates[i].candidateAddress,
                             true
                         )
                     );
@@ -104,34 +106,28 @@ contract BlockchainVoting {
     function getRandomNormalized() public view returns (uint256) {
         // Generate a pseudo-random number using keccak256 hash
         uint256 randomNumber = uint256(keccak256(abi.encode(block.timestamp, block.difficulty, msg.sender)));
-
         // Use modulo to limit the range of the random number from 0 to 100
         return randomNumber % 101;
     }
 
-
-
-
-
 function publishResults() public view returns (Results[] memory) {
     Results[] memory finalResults = new Results[](2);
 
-    // Calculating votes based on some given logic
-    uint256 total_number_of_votes_for_x = (4 * candidates[0].voteCount - 1) / 2;
-    uint256 total_number_of_votes_for_y = candidates[1].voteCount - (total_number_of_votes_for_x - candidates[0].voteCount);
-
+    
+    if(candidates.length>0){
+        uint256 total_number_of_votes_for_x = (4 * candidates[0].voteCount - 1) / 2;
+        uint256 total_number_of_votes_for_y = candidates[1].voteCount - (total_number_of_votes_for_x - candidates[0].voteCount);
     finalResults[0] = Results(
-        candidates[0].name,
-        total_number_of_votes_for_x
+         candidates[0].name,
+       total_number_of_votes_for_x
     );
     finalResults[1] = Results(
         candidates[1].name,
-        total_number_of_votes_for_y
+       total_number_of_votes_for_y
     );
-
+    }
     return finalResults;
 }
-
 
     function getCandidates() external view returns (Candidate[] memory) {
         return candidates;
